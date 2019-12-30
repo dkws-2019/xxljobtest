70add4996048d60539401e36a43d1338572ae513
1005929194@qq.com <1005929194@qq.com>
2019-12-30 14:42:56 +0800

    Initial commit

---
+++ src/main/java/com/liuchao/xxljobtest/XxljobtestApplication.java
@@ -0,0 +1,13 @@
+package com.liuchao.xxljobtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxljobtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XxljobtestApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/liuchao/xxljobtest/executor.java
@@ -0,0 +1,4 @@
+package com.liuchao.xxljobtest;
+
+public class executor {
+}
